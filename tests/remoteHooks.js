37ff4c10963d3c1e9d9fa1d72ab9332dc4c0f5ca
Elmar Langholz <langholz@gmail.com>
2014-01-17 14:17:15 -0800

    Removing remote hooks tests commenting

--- tests/remoteHooks.js
+++ tests/remoteHooks.js
@@ -1,7 +1,25 @@
-/*var should = require('should');
+var should = require('should');
+var http = require('http');
+var HttpHooks = require('../lib/httphooks.js');
 
 describe('HttpHooks', function () {
     describe('#remoteHooks', function() {
-        describe('')
+        var httpServer1 = null;
+        var httpHooks1 = null;
+        var httpServer2 = null;
+        var httpHooks2 = null;
+
+        beforeEach(function (done) {
+            httpHooks1 = new HttpHooks();
+            httpHooks2 = new HttpHooks();
+        });
+
+        afterEach(function (done) {
+
+        });
+
+        it('should ', function () {
+
+        });
     });
-});*/
+});
